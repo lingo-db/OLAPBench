c4c1b6d77cb9e6e84ef9c9d5d16b9a734a076997
Lukas Döllerer <lukas.doellerer@tum.de>
2025-05-23 15:41:00 +0200

    adjust job benchmark dbgen script for macos

--- benchmarks/job/dbgen.sh
+++ benchmarks/job/dbgen.sh
@@ -12,14 +12,29 @@ if [ ! -d "job" ]; then
     mkdir job
     cd job
 
-    echo '552a24e5bbe0b9bd727649200294bbac imdb.tzst' | md5sum --check --status 2>/dev/null || curl -OL https://db.in.tum.de/~schmidt/dbgen/job/imdb.tzst
-    echo '552a24e5bbe0b9bd727649200294bbac imdb.tzst' | md5sum --check --status
-    tar --skip-old-files -xf imdb.tzst
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      if [ ! -f imdb.tzst ] || ! [ "$(md5 -q imdb.tzst 2>/dev/null)" = "552a24e5bbe0b9bd727649200294bbac" ]; then
+        curl -OL https://db.in.tum.de/~schmidt/dbgen/job/imdb.tzst
+      fi
+      [ "$(md5 -q imdb.tzst)" = "552a24e5bbe0b9bd727649200294bbac" ] || { echo "MD5 mismatch"; exit 1; }
+      tar -xf imdb.tzst
+    else
+      echo '552a24e5bbe0b9bd727649200294bbac imdb.tzst' | md5sum --check --status 2>/dev/null || curl -OL https://db.in.tum.de/~schmidt/dbgen/job/imdb.tzst
+      echo '552a24e5bbe0b9bd727649200294bbac imdb.tzst' | md5sum --check --status
+      tar --skip-old-files -xf imdb.tzst
+    fi
     rm imdb.tzst
 
-    for file in *.csv; do
-      sed -E -i 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$file"
-      sed -E -i 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$file"
-    done
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      for table in ./*.csv; do
+        sed -E -i '' 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$table"
+        sed -E -i '' 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$table"
+      done
+    else
+      for table in ./*.csv; do
+        sed -E -i 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$table"
+        sed -E -i 's/([^\\])(\\{2})*(\\)(")/\1\2""/g' "$table"
+      done
+    fi
   )
 fi
